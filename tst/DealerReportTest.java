70f522d1c1697b6c97c40c740524639413b277d6
saidbrandonsaid <brandonmatthew.said@calbaptist.edu>
2021-11-02 12:08:13 -0700

    Added two additional tests: One with a large inventory and one where generateReport is used on a blank file. All tests work properly.

--- tst/DealerReportTest.java
+++ tst/DealerReportTest.java
@@ -1,8 +1,5 @@
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,19 +15,11 @@ public class DealerReportTest {
      * a test ran.
      */
     @Before
-    public void destroyFileBefore() {
+    public void destroyFileBeforeTests() {
         File reportFile = new File("report.txt");
         reportFile.delete();
         //NOTE: Might require if/else based on if report.txt exists
     }
-
-    /*
-    @After
-    public void destroyFileAfter() {
-        File reportFile = new File("report.txt");
-        reportFile.delete();
-    }
-     */
 
 
     /**
@@ -60,6 +49,35 @@ public class DealerReportTest {
         Assert.assertTrue("File does not exist.", firstFile.exists());
     }
 
+    /**
+     * Tests that generateReport() produces the correct file after
+     */
+    @Test
+    public void generateReportWithFullInventoryTest() throws IOException {
+        // Arrange
+        Inventory firstInventory = new Inventory();
+        Vehicle vehicle1 = new Vehicle("Ford F150",2015,35000,true);
+        Vehicle vehicle2 = new Vehicle("Ford Focus",2010,15000,false);
+        Vehicle vehicle3 = new Vehicle("Ford Fiesta",2012,18000,false);
+        Vehicle vehicle4 = new Vehicle("GM Sierra",2016,40000,true);
+        Vehicle vehicle5 = new Vehicle("Chevy Silverado",2016,35000,true);
+        firstInventory.add(vehicle1);
+        firstInventory.add(vehicle2);
+        firstInventory.add(vehicle3);
+        firstInventory.add(vehicle4);
+        firstInventory.add(vehicle5);
+
+        Dealer firstDealer = new Dealer();
+        File report = new File("report.txt");
+        File expectedReport = new File("generateReportWithFullInventory.txt");
+
+        // Act
+        firstDealer.generateReport(firstInventory);
+
+        // Assert
+        Assert.assertTrue("File generated incorrectly.", FileUtils.contentEquals(report, expectedReport));
+    }
+
 
 
     /**
@@ -82,13 +100,32 @@ public class DealerReportTest {
 
 
         //Assert
-        Assert.assertTrue("Files do not match.", FileUtils.contentEquals(report, expectedReport));
+        Assert.assertTrue("File's text generated incorrectly.",
+                FileUtils.contentEquals(report, expectedReport));
     }
 
 
     /**
      * Test checks that appending works when file exists, but is blank to start.
      */
+    @Test
+    public void appendingToBlankFileTest() throws IOException {
+        //Arrange
+        File report = new File("report.txt"); //Should create blank file?
+        Inventory inventory = new Inventory();
+        inventory.add(new Vehicle("Ford Fiesta",2012,18000,false));
 
+        File expectedReport = new File("appendingToBlankFile.txt");
+
+        //Act
+        Dealer dealer = new Dealer();
+        dealer.generateReport(inventory);
+
+        //Assert
+        Assert.assertTrue("File's text generated incorrectly.",
+                FileUtils.contentEquals(report, expectedReport));
+    }
+
+    //Could have null test
 
 }
